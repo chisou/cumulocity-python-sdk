7a8705c6235084f349217f6b162d4c010cd41f32
Christoph Souris <christoph.souris@cumulocity.com>
2026-02-12 12:15:25 +0100

    Adding matchers.

---
+++ pyc8y/model/matcher/__init__.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2026 Christoph Souris
+
+from ._matcher import (
+    JsonMatcher,
+    AllMatcher,
+    match_all,
+    AnyMatcher,
+    match_any,
+    NotMatcher,
+    match_not,
+    FragmentMatcher,
+    fragment,
+    FieldMatcher,
+    field,
+    DescriptionMatcher,
+    description,
+    TextMatcher,
+    text,
+    CommandMatcher,
+    command,
+)
+
+try:
+    import pydictdisplayfilter as _pydf
+    from ._pydf import PydfMatcher, pydf
+except ImportError:
+    pass
+
+try:
+    import jmespath as _jmespath
+    from ._jmespath import JmesPathMatcher, jmespath
+except ImportError:
+    pass
+
+try:
+    import jsonpath_ng as _jsonpath_ng
+    from ._jsonpath import JsonPathMatcher, jsonpath
+except ImportError:
+    pass
