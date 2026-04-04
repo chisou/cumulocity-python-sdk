cd01643ec702b00d9cc9e7acf6234b2ee9a870ec
Christoph Souris <christoph.souris@cumulocity.com>
2026-04-04 16:15:03 +0200

    Migrated operations integration tests to async.

--- integration_tests/test_bulk_operations.py
+++ integration_tests/test_bulk_operations.py
@@ -1,71 +1,74 @@
-# Copyright (c) 2025 Cumulocity GmbH
+# Copyright (c) 2026 Christoph Souris
 
-import time
+import asyncio
 
-from c8y_api import CumulocityApi
-from c8y_api.model import BulkOperation, DeviceGroup, Operation
-from c8y_api.model.matcher import field
+from pyc8y.client import CumulocityClient
+from pyc8y.model.managed_object import Device, DeviceGroup
+from pyc8y.model.matcher import field
+from pyc8y.model.operation import BulkOperation, Operation, BulkStatus, GeneralBulkStatus, OperationStatus
 
 
-def test_CRU(live_c8y: CumulocityApi, session_device):  # noqa
-    """Verify that basic creation, lookup and update of Operations works as expected."""
+async def test_CRU(live_c8y: CumulocityClient, session_device: Device):  # noqa (case)
+    """Verify that basic creation, lookup and update of Bulk Operations works as expected."""
 
     # (1) Create a device group for the sample device
-    group:DeviceGroup = DeviceGroup(live_c8y,
-                                    root=True,
-                                    name=session_device.name + '_Group').create()
-    group.add_child_asset(session_device)
+    group: DeviceGroup = await DeviceGroup(live_c8y, root=True, name=session_device.name + '_Group').create()
+    await group.add_child_asset(session_device)
 
+    try:
+        # (2) create bulk operation
+        bulk: BulkOperation = await BulkOperation(
+            live_c8y,
+            group_id=group.id,
+            start_time='now',
+            creation_ramp=1,
+            operation_prototype={
+                'description': f"Update firmware for device group '{group.name}'.",
+                'c8y_FirmWare': {'version': '1.0.0'},
+            },
+        ).create()
 
-    # (2) create bulk operation
-    bulk:BulkOperation = BulkOperation(live_c8y,
-                          group_id=group.id,
-                          start_time='now',
-                          creation_ramp=1,
-                          operation_prototype={
-                              'description': f"Update firmware for device group '{group.name}'.",
-                              'c8y_FirmWare': {
-                                  'version': '1.0.0'
-                              }}
-                          ).create()
+        # wait for the bulk operation to be processed
+        await asyncio.sleep(5)
 
-    # wait for the bulk operation to be processed
-    time.sleep(5)
+        # check if bulk operation was created
+        all_ids = [x.id for x in await live_c8y.bulk_operations.get_all()]
+        assert bulk.id in all_ids
 
-    # Check if bulk operation was created
-    all_ids = [x.id for x in live_c8y.bulk_operations.get_all()]
-    assert bulk.id in all_ids
+        # use client-side filtering
+        assert bulk.id in [
+            x.id for x in await live_c8y.bulk_operations.get_all(include=field("groupId", group.id))
+        ]
 
-    # use client side filtering
-    assert bulk.id in [x.id for x in live_c8y.bulk_operations.get_all(include=field('groupId', group.id))]
+        # check count
+        assert len(all_ids) == await live_c8y.bulk_operations.get_count()
 
-    # check count
-    assert len(all_ids) == live_c8y.bulk_operations.get_count()
+        # (3) initially the status should be EXECUTING/COMPLETED as all
+        #     child operations should have been created but not completed
+        bulk = await live_c8y.bulk_operations.get(bulk.id)
+        assert bulk.general_status == GeneralBulkStatus.EXECUTING
+        assert bulk.status == BulkStatus.COMPLETED
+        assert bulk['progress']['all'] == 1
+        assert bulk['progress']['pending'] == 1
 
-    # (3) initially the status should be EXECUTING/COMPLETED as all
-    #     child operations should have been created but not completed
-    bulk = live_c8y.bulk_operations.get(bulk.id)
-    assert bulk.general_status == BulkOperation.GeneralStatus.EXECUTING
-    assert bulk.status == BulkOperation.Status.COMPLETED
-    assert bulk.progress.all == 1
-    assert bulk.progress.pending == 1
+        # (4) find child operations
+        op = (await live_c8y.operations.get_all(bulk_id=bulk.id))[0]
+        assert op.status == OperationStatus.PENDING
 
-    # (4) find child operations
-    op = live_c8y.operations.get_all(bulk_id=bulk.id)[0]
-    assert op.status == Operation.Status.PENDING
+        # (5) fail child operation
+        op.status = OperationStatus.FAILED
+        await op.update()
 
-    # (5) kill child operations
-    op.status = Operation.Status.FAILED
-    op.update()
+        # (6) bulk operation should now reflect the failure
+        bulk = await live_c8y.bulk_operations.get(bulk.id)
+        assert bulk.general_status in [
+            GeneralBulkStatus.COMPLETED_WITH_FAILURES,
+            GeneralBulkStatus.FAILED,
+        ]
+        assert bulk['progress']['all'] == 1
+        assert bulk['progress']['failed'] == 1
+        assert await live_c8y.operations.get_count(bulk_id=bulk.id) == 1
 
-    # (5) bulk operation should now be in PENDING state
-    bulk = live_c8y.bulk_operations.get(bulk.id)
-    assert bulk.general_status in [BulkOperation.GeneralStatus.COMPLETED_WITH_FAILURES,
-                                   BulkOperation.GeneralStatus.FAILED]
-    assert bulk.progress.all == 1
-    assert bulk.progress.failed == 1
-    assert live_c8y.operations.get_count(bulk_id=bulk.id) == 1
-
-    # (6) cleanup
-    #     The bulk operation cannot be deleted physically
-    group.delete()
+    finally:
+        # bulk operations cannot be deleted physically
+        await group.delete()
--- integration_tests/test_operations.py
+++ integration_tests/test_operations.py
@@ -1,92 +1,106 @@
-# Copyright (c) 2025 Cumulocity GmbH
+# Copyright (c) 2026 Christoph Souris
 
-from c8y_api import CumulocityApi
-from c8y_api.model import Operation
+import asyncio
+
+from pyc8y.client import CumulocityClient
+from pyc8y.model.managed_object import Device
+from pyc8y.model.operation import Operation, OperationStatus
 
 from util.testing_util import RandomNameGenerator
 
 
-def test_CRUD(live_c8y: CumulocityApi, session_device):
+async def test_CRUD(live_c8y: CumulocityClient, session_device: Device):  # noqa (case)
     """Verify that basic creation, lookup and update of Operations works as expected."""
 
     name = RandomNameGenerator.random_name()
 
     # (1) create operation
-    operation = Operation(live_c8y, session_device.id, description='Description ' + name,
-                          c8y_Command={'text': 'Command text'})
-    operation = operation.create()
+    operation = await Operation(
+        live_c8y,
+        device_id=session_device.id,
+        description=f'Description {name}',
+        c8y_Command={'text': 'Command text'},
+    ).create()
 
     # -> operation should have been created and in PENDING state
-    operations = live_c8y.operations.get_all(device_id=session_device.id, status=Operation.Status.PENDING)
+    operations = await live_c8y.operations.get_all(device_id=session_device.id, status=OperationStatus.PENDING)
     assert len(operations) == 1
     assert operations[0].id == operation.id
 
     # -> same result with get_last
-    operation2 = live_c8y.operations.get_last(device_id=session_device.id, status=Operation.Status.PENDING)
+    operation2 = await live_c8y.operations.get_last(device_id=session_device.id, status=OperationStatus.PENDING)
     assert operation2.id == operation.id
 
     # (2) update operation
-    operation.status = Operation.Status.EXECUTING
+    operation.status = OperationStatus.EXECUTING
     operation.description = 'New description'
-    operation.c8y_Command.text = 'Updated command text'
-    operation.add_fragment('c8y_CustomCommand', value='good')
-    operation.update()
+    operation['c8y_Command'] = {'text': 'Updated command text'}
+    operation['c8y_CustomCommand'] = {'value': 'good'}
+    await operation.update()
 
     # -> all fields have been updated in Cumulocity
-    operation2 = live_c8y.operations.get(operation.id)
+    operation2 = await live_c8y.operations.get(operation.id)
     assert operation2.status == operation.status
     assert operation2.description == operation.description
-    assert operation2.c8y_Command.text == operation.c8y_Command.text
-    assert operation2.c8y_CustomCommand.value == operation.c8y_CustomCommand.value
+    assert operation2['c8y_Command.text'] == operation['c8y_Command.text']
+    assert operation2['c8y_CustomCommand.value'] == operation['c8y_CustomCommand.value']
 
     # (3) delete operation
-    live_c8y.operations.delete_by(device_id=session_device.id)
+    await live_c8y.operations.delete_by(device_id=session_device.id)
 
     # -> cannot be found anymore
-    assert not live_c8y.operations.get_all(device_id=session_device.id)
+    assert not await live_c8y.operations.get_all(device_id=session_device.id)
 
 
-def test_get(live_c8y: CumulocityApi, session_device):
+async def test_get(live_c8y: CumulocityClient, session_device: Device):
     """Verify that query-like retrieval works as expected."""
+
     # (1) create operations
-    operations = [
-        Operation(live_c8y, session_device.id, description=f'Description {i}',
-                  c8y_Command={'text': 'Command text'}).create()
+    operations = list(await asyncio.gather(*[
+        Operation(
+            live_c8y,
+            device_id=session_device.id,
+            description=f'Description {i}',
+            c8y_Command={'text': 'Command text'},
+        ).create()
         for i in range(5)
-    ]
+    ]))
 
-    # (2) all should have been created an in PENDING state
-    result = live_c8y.operations.get_all(device_id=session_device.id, status=Operation.Status.PENDING)
-    assert len(result) == 5
-    assert all(o.device_id == session_device.id for o in result)
+    try:
+        # (2) all should have been created and in PENDING state
+        result = await live_c8y.operations.get_all(device_id=session_device.id, status=OperationStatus.PENDING)
+        assert len(result) == 5
+        assert all(o.device_id == session_device.id for o in result)
 
-    # (3) get last
-    result = live_c8y.operations.get_last(device_id=session_device.id)
-    assert isinstance(result, Operation)
-    assert result.device_id == session_device.id
+        # (3) get last
+        result = await live_c8y.operations.get_last(device_id=session_device.id)
+        assert isinstance(result, Operation)
+        assert result.device_id == session_device.id
 
-    # (4) retrieving subsets
-    operations[0].status = Operation.Status.EXECUTING
-    operations[1].status = Operation.Status.EXECUTING
-    operations[0].update()
-    operations[1].update()
+        # (4) retrieving subsets
+        operations[0].status = OperationStatus.EXECUTING
+        operations[1].status = OperationStatus.EXECUTING
+        await asyncio.gather(operations[0].update(), operations[1].update())
 
-    result = live_c8y.operations.get_all(device_id=session_device.id, status=Operation.Status.PENDING)
-    assert len(result) == 3
-    result = live_c8y.operations.get_last(device_id=session_device.id, status=Operation.Status.PENDING)
-    assert result.status == Operation.Status.PENDING
-    assert result.device_id == session_device.id
+        result = await live_c8y.operations.get_all(device_id=session_device.id, status=OperationStatus.PENDING)
+        assert len(result) == 3
+        result = await live_c8y.operations.get_last(device_id=session_device.id, status=OperationStatus.PENDING)
+        assert result.status == OperationStatus.PENDING
+        assert result.device_id == session_device.id
 
-    result = live_c8y.operations.get_all(device_id=session_device.id, status=Operation.Status.EXECUTING)
-    assert len(result) == 2
-    result = live_c8y.operations.get_last(device_id=session_device.id, status=Operation.Status.EXECUTING)
-    assert result.status == Operation.Status.EXECUTING
-    assert result.device_id == session_device.id
+        result = await live_c8y.operations.get_all(device_id=session_device.id, status=OperationStatus.EXECUTING)
+        assert len(result) == 2
+        result = await live_c8y.operations.get_last(device_id=session_device.id, status=OperationStatus.EXECUTING)
+        assert result.status == OperationStatus.EXECUTING
+        assert result.device_id == session_device.id
 
-    # (5) deleting subsets
-    live_c8y.operations.delete_by(device_id=session_device.id, status=Operation.Status.EXECUTING)
-    assert live_c8y.operations.get_all(device_id=session_device.id, status=Operation.Status.EXECUTING) == []
-    assert len(live_c8y.operations.get_all(device_id=session_device.id, status=Operation.Status.PENDING)) == 3
+        # (5) deleting subsets
+        await live_c8y.operations.delete_by(device_id=session_device.id, status=OperationStatus.EXECUTING)
+        assert await live_c8y.operations.get_all(device_id=session_device.id, status=OperationStatus.EXECUTING) == []
+        assert len(await live_c8y.operations.get_all(device_id=session_device.id, status=OperationStatus.PENDING)) == 3
 
-    # (6) no match with get_last
-    assert live_c8y.operations.get_last(device_id=session_device.id, status=Operation.Status.EXECUTING) is None
+        # (6) no match with get_last
+        assert await live_c8y.operations.get_last(device_id=session_device.id, status=OperationStatus.EXECUTING) is None
+
+    finally:
+        await live_c8y.operations.delete_by(device_id=session_device.id)
